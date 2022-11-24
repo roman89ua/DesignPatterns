20d1bb040dbc5881146213b52c36dcadf3221949
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2022-11-25 01:03:48 +0200

    Separate folder for behavioral patterns.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { Memento } from "./patterns/Memento/Memento";
 /**
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
 import confetti from "canvas-confetti";
-import { State } from "./patterns/State/State";
-import { Iterator } from "./patterns/Iterator/Iterator";
-import { Strategy } from "./patterns/Strategy/Strategy";
-import { TemplateMethod } from "./patterns/TemplateMethod/TemplateMethod";
-import {Command} from "./patterns/Command/Command";
-import {Observer} from "./patterns/Observer/Observer";
+import { Memento } from "./patterns/Behavioral/Memento/Memento";
+import { State } from "./patterns/Behavioral/State/State";
+import { Iterator } from "./patterns/Behavioral/Iterator/Iterator";
+import { Strategy } from "./patterns/Behavioral/Strategy/Strategy";
+import { TemplateMethod } from "./patterns/Behavioral/TemplateMethod/TemplateMethod";
+import { Command } from "./patterns/Behavioral/Command/Command";
+import { Observer } from "./patterns/Behavioral/Observer/Observer";
+import { Mediator } from "./patterns/Behavioral/Mediator/Mediator";
 
 confetti.create(document.getElementById("canvas") as HTMLCanvasElement, {
   resize: true,
   useWorker: true,
 })({ particleCount: 200, spread: 200 });
 
-const memento = new Memento();
-const state = new State("Red", "1px");
-const iterator = new Iterator();
-const strategy = new Strategy();
-const templateMethod = new TemplateMethod();
-const command = new Command();
-const observer = new Observer();
+new Memento();
+new State("Red", "1px");
+new Iterator();
+new Strategy();
+new TemplateMethod();
+new Command();
+new Observer();
+new Mediator();
